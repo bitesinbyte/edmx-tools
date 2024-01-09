d2f4a81db2bb0cd7335442b409a3e8a0382965c0
manishtiwari25 <manish.ertiwari@outlook.com>
2024-01-09 18:26:42 +0100

    Added conversion

---
+++ src/Helpers/OpenApiHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Csdl;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.OData;
+
+namespace EdmxTools;
+
+public static class OpenApiHelper
+{
+    public static string Convert(string xml, OpenApiFormat openApiFormat)
+    {
+        var settings = new OpenApiConvertSettings
+        {
+            OpenApiSpecVersion = OpenApiSpecVersion.OpenApi3_0
+        };
+        return Generate(xml, openApiFormat, settings);
+    }
+    private static string Generate(string input, OpenApiFormat format, OpenApiConvertSettings settings)
+    {
+        var edmModel = GetEdmModel(input);
+        var document = edmModel.ConvertToOpenApi(settings);
+        return document.Serialize(settings.OpenApiSpecVersion, format);
+    }
+    private static IEdmModel GetEdmModel(string input)
+    {
+        var parsed = XElement.Parse(input);
+        using XmlReader mainReader = parsed.CreateReader();
+        return CsdlReader.Parse(mainReader, u =>
+        {
+            var referenceParsed = XElement.Parse(input);
+            var referenceReader = referenceParsed.CreateReader();
+            return referenceReader;
+        });
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddRadzenComponents();
 await builder.Build().RunAsync();
